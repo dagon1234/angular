897a9793962d284dea14c45e03196dddf6c00aab
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-07-30 13:37:47 +0700

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CaptionItem } from './caption-item';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  title: string | undefined;
+  count: number;
+  imgSrc: string = "./assets/angular_logo.png";
+
+  captionList: CaptionItem[] = [
+    {
+      id: 1,
+      message: 'สมัยนี้พรหมลิขิต ก็สู้บัตรเครดิตไม่ได้',
+      icon: './assets/icons/ic_funny_01.png',
+    },
+    {
+      id: 2,
+      message: 'หน้าหนาวทำให้ปากแตก แต่เดี๋ยวมึงได้หน้าแหกถ้าปากดี',
+      icon: './assets/icons/ic_funny_02.png',
+    },
+    {
+      id: 3,
+      message: 'อย่าโง่นัก ซึ่งมันอาจทำให้คุณโด่งดังได้',
+      icon: './assets/icons/ic_funny_03.png',
+    },
+  ];
+
+  usedCaptionList: CaptionItem[] = [];
+
+  constructor() {
+    this.title = this.randomCaption()?.message;
+    this.count = 0;
+  }
+
+  randomCaption() {
+    let randomIndex: number;
+    let newCaption: CaptionItem;
+    this.count++;
+
+    if (this.usedCaptionList.length === this.captionList.length) {
+      return null
+    }
+
+    do {
+      randomIndex = this.getRandomInt(this.captionList.length);
+      newCaption = this.captionList[randomIndex]
+    } while (this.usedCaptionList.includes(newCaption));
+
+    this.usedCaptionList.push(newCaption);
+    return this.captionList[randomIndex];
+  }
+
+
+  handleClickButton() {
+    this.title = this.randomCaption()?.message;
+  }
+
+  private getRandomInt(max: number) {
+    return Math.floor(Math.random() * max)
+  }
 }
---
+++ src/app/caption-item.ts
@@ -0,0 +1,6 @@
+//ng generate interface captionItem(in terminal)
+export interface CaptionItem {
+    id: number;
+    message: string;
+    icon: string;
+}
